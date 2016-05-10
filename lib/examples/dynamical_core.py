e23a710037e2f197e3e7fa7b1e29a10730d2f73a
Joy M. Monteiro <joy.merwin@gmail.com>
2016-05-10 17:18:45 +0530

    example script to run dynamical core without any forcing

--- lib/examples/dynamical_core.py
+++ lib/examples/dynamical_core.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from numpy import *
-from climt import *
+from dynamics import *
 from pylab import *
 from myCmap import *
 
@@ -10,32 +10,25 @@ kwargs = {}
 kwargs['afc'] = 0.4
 kwargs['dt'] = 600.
 
-nhours = 1000
+nhours = 100
 nsteps = int((nhours*3600)/kwargs['dt'])
 print 'Num Steps: ', nsteps
 
 dycore = dynamics(scheme='gfs', **kwargs)
 
+print '==========================='
 oldU = 0
 newU = 0
 incU = 0
 
-for i in range(nsteps):
-    oldU = dycore['U'].copy()
 
+for i in range(nsteps):
+
+    
+    print '==========================='
     dycore.step()
 
-    incU = dycore.Inc['U'].copy()
-    newU = dycore['U'].copy()
 
-contourf(average(oldU - newU + incU, axis=0))
-colorbar()
-figure()
-zonalmean = average(newU, axis=0).transpose()
-maxLev = amax(abs(zonalmean))
-levs = linspace(-maxLev, maxLev, 16)
-contourf(zonalmean ,levels = levs, cmap=joyDivCmapRdBl, origin='lower')
-colorbar()
-show()
 
 dycore.Extension.shutDownModel()
+
