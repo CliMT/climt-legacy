5797b974248b4a5d8cc7ef5d24e61f240387b2f1
Joy M. Monteiro <joy.merwin@gmail.com>
2016-06-12 08:49:59 +0200

    added new module dcmip for initial conditions for the Jablonowski and Williams tests

---
+++ src/idealised/dcmip/dcmip_initial_conditions.pyx
@@ -0,0 +1,86 @@
+cimport numpy as cnp
+import numpy as np
+
+#DCMIP function in the fortran code which returns
+#the initial conditions for the baroclinic wave test
+cdef extern:
+    void dcmipBaroclinicWave(int *moist,double *X,\
+                             double *lon, double *lat,\
+                             double *p, double *z,\
+                             int *zcoords,\
+                             double * u, double * v,\
+                             double * w, double * t,\
+                             double * phis, double *ps,\
+                             double * rho, double *q,\
+                             double * q1, double *q2, int* perturb)
+
+#What we are sending to the code
+cdef double latitude, longitude, pressure
+cdef int moist
+cdef int add_perturbation
+
+#What we want from the code
+cdef double u, v, t, phis
+
+#What we dont care about
+cdef double dummy, X
+cdef int dummy_int, zcoords
+
+def getBaroclinicWaveICs(press, lon, lat, **kwargs):
+    '''
+    Provides the initial conditions to run the DCMIP baroclinic
+    wave test
+
+    '''
+
+    global longitude, latitude, pressure, moist, zcoords, add_perturbation
+
+    num_lons, num_lats, num_levs = press.shape
+
+    zonal_vel = np.zeros(press.shape, dtype=np.double, order='F')
+    merid_vel = np.zeros(press.shape, dtype=np.double, order='F')
+    temperature = np.zeros(press.shape, dtype=np.double, order='F')
+    surf_geop = np.zeros((num_lons,num_lats), dtype=np.double, order='F')
+
+    # This makes the code evaluate eta using the pressure.
+    zcoords = 0
+
+    if 'moist' in kwargs:
+        moist = kwargs['moist']
+    else:
+        moist = 0
+
+    add_perturbation = 0
+
+    if 'perturb' in kwargs:
+        if kwargs['perturb'] is True:
+            add_perturbation = 1
+
+
+
+    #Scale factor, not used in code, but present in argument list!
+    X = 1.
+
+    # lat-lon in RADIANS!!
+
+    for i in range(num_lons):
+        for j in range(num_lats):
+            for k in range(num_levs):
+                longitude = lon[i,j]
+                latitude = lat[i,j]
+                pressure = press[i,j,k]
+                dcmipBaroclinicWave(&moist, &X,\
+                                    &longitude, &latitude,\
+                                    &pressure, &dummy,\
+                                    &zcoords, \
+                                    &u, &v,\
+                                    &dummy, &t,\
+                                    &phis, &dummy,\
+                                    &dummy, &dummy,\
+                                    &dummy, &dummy, &add_perturbation)
+                zonal_vel[i,j,k] = u
+                merid_vel[i,j,k] = v
+                temperature[i,j,k] = t
+                surf_geop[i,j] = phis
+
+    return zonal_vel, merid_vel, temperature, surf_geop
---
+++ src/idealised/dcmip/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+# This line only needed if building with NumPy in Cython file.
+from numpy import get_include
+from os import system
+
+# compile the fortran modules without linking
+fortran_mod_comp = 'gfortran dcmip_initial_conditions_test_4_v3.f90 -c -o dcmip_test4.o -O3 -fPIC'
+print fortran_mod_comp
+system(fortran_mod_comp)
+
+ext_modules = [Extension(# module name:
+                         'dcmip',
+                         # source file:
+                         ['dcmip_initial_conditions.pyx'],
+                         # other compile args for gcc
+                         extra_compile_args=['-fPIC', '-O3'],
+                         # other files to link to
+                         extra_link_args=['dcmip_test4.o'])]
+
+setup(name = 'dcmip',
+      cmdclass = {'build_ext': build_ext},
+      # Needed if building with NumPy.
+      # This includes the NumPy headers when compiling.
+      include_dirs = [get_include()],
+      ext_modules = ext_modules)
